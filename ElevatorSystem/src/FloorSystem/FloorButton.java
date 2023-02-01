86552d51b4ce6f9a9620e589cf29d0a7c0f4c2eb
Keefer Belanger <keefer.belanger@gmail.com>
2023-02-01 17:14:28 -0500

    Changed floor numbers to strings to be taken from input file

--- ElevatorSystem/src/FloorSystem/FloorButton.java
+++ ElevatorSystem/src/FloorSystem/FloorButton.java
@@ -16,8 +16,8 @@ public class FloorButton {
 	
 	private String time;
 	private Direction direction;
-	private int currFloorNum;
-	private int destFloorNum;
+	private String currFloorNum;
+	private String destFloorNum;
 	
 	/**
 	 * Constructor for FloorButton class
@@ -26,7 +26,7 @@ public class FloorButton {
 	 * @param destFloorNum
 	 * @param direction
 	 */
-	public FloorButton(String time, int currFloorNum, Direction direction, int destFloorNum) {
+	public FloorButton(String time, String currFloorNum, Direction direction, String destFloorNum) {
 		this.time = time;
 		this.currFloorNum = currFloorNum;
 		this.destFloorNum = destFloorNum;
@@ -53,7 +53,7 @@ public class FloorButton {
 	 * Getter method for the current floor
 	 * @return an int that shows the elevators current floor
 	 */
-	public int getCurrFloorNum() {
+	public String getCurrFloorNum() {
 		return currFloorNum;
 	}
 	
@@ -61,7 +61,7 @@ public class FloorButton {
 	 * Setter method for the current floor
 	 * @param currFloorNum : the current floor the elevator is on
 	 */
-	public void setCurrFloorNum(int currFloorNum) {
+	public void setCurrFloorNum(String currFloorNum) {
 		this.currFloorNum = currFloorNum;
 	}
 	
@@ -85,7 +85,7 @@ public class FloorButton {
 	 * Getter method for the destination number where the elevator wants to move to
 	 * @return the destination floor
 	 */
-	public int getDestFloorNum() {
+	public String getDestFloorNum() {
 		return destFloorNum;
 	}
 	
@@ -93,7 +93,7 @@ public class FloorButton {
 	 * Setter method for the selected floor
 	 * @param destFloorNum : floor number destination
 	 */
-	public void setDestFloorNum(int destFloorNum) {
+	public void setDestFloorNum(String destFloorNum) {
 		this.destFloorNum = destFloorNum;
 	}
 	
