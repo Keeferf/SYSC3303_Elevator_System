2fde77732d9bf8aadbb6c84e26ef841395a914d8
Joshua <josh16noronha@gmail.com>
2023-02-01 21:15:19 -0500

    Added more methods

--- Elevator.java
+++ Elevator.java
@@ -5,25 +5,34 @@ import java.util.ArrayList;
 
 public class Elevator {
 	
-	private int maxFloor = 2;
-	private int groundFloor = 0;
+	private int maxFloor;
+	private int groundFloor;
 	private int curFloor;
 	private int floorToGo;
-	private ArrayList<Floor.Rider> contents = null; // contents
-	private boolean empty = true; // empty?
+	//private ArrayList<Floor.Rider> contents = null; // contents
+	//private boolean empty = true; // empty?
 	
-	public Elevator(int floorToGo, int curFloor) {
+	public Elevator(int floorToGo, int curFloor, int maxFloor, int groundFloor) {
 		this.floorToGo = floorToGo;
 		this.curFloor = curFloor;
+		this.maxFloor = maxFloor;
+		this.groundFloor = groundFloor;
 	}
 	
-	public void up() {
+	/**
+	 * Moves the Elevator up to the floor the passenger needs to go.
+	 * 
+	 * @throws InterruptedException
+	 */
+	private void up() throws InterruptedException {
 		
-		if (curFloor == maxFloor && floorToGo == maxFloor) {
+		if (curFloor == floorToGo) {
 			System.out.print("Doors Open");
+			Thread.sleep(1318);
 		}
 		else{
 			System.out.print("Doors Closed");
+			Thread.sleep(1318);
 			while(curFloor != floorToGo) {
 				curFloor++;
 			}
@@ -31,13 +40,20 @@ public class Elevator {
 		}
 	}
 	
-	public void down() {
+	/**
+	 * Moves the Elevator down to the floor the passenger needs to go.
+	 * 
+	 * @throws InterruptedException
+	 */
+	private void down() throws InterruptedException {
 		
-		if (curFloor == groundFloor && floorToGo == groundFloor) {
+		if (curFloor == floorToGo) {
 			System.out.print("Doors Open");
+			Thread.sleep(1318);
 		}
 		else{
 			System.out.print("Doors Closed");
+			Thread.sleep(1318);
 			while(curFloor != floorToGo) {
 				curFloor--;
 			}
@@ -45,7 +61,24 @@ public class Elevator {
 		}
 	}
 	
-	 public synchronized void put(ArrayList<Floor.Rider> items) {
+	/**
+	 * Moves the Elevator up or down depending on the floor the passenger's request.
+	 * 
+	 * @throws InterruptedException
+	 */
+	public void pressButton(Schedule sch) throws InterruptedException {
+		if (floorToGo < curFloor) {
+			down();
+		}
+		else {
+			up();
+		}
+		
+	}
+	
+	
+	
+	/* public synchronized void put(ArrayList<Floor.Rider> items) {
 	        while (!empty) {
 	            try {
 	                wait();
@@ -75,22 +108,65 @@ public class Elevator {
         notifyAll();
         return items;
     }
-	 
 	
+	*/
+	
+	/**
+	 * Returns the Current floor that the Elevator is on.
+	 * 
+	 * @return curFloor
+	 */
 	public int getCurFloor() {
 		return curFloor;
 	}
 	
+	/**
+	 * Returns true if the current floor of the Elevator is set.
+	 * 
+	 * @return true
+	 */
+	public boolean setCurFloor(int newCurFloor) {
+		this.curFloor = newCurFloor;
+		return true;
+		
+	}
+	/**
+	 * Returns the ground floor of the Elevator.
+	 * 
+	 * @return groundFloor
+	 */
 	public int getGroundFloor() {
 		return groundFloor;
 	}
 	
+	/**
+	 * Returns the maximum floor of the Elevator.
+	 * 
+	 * @return maxFloor
+	 */
 	public int getMaxFloor() {
 		return maxFloor;
 	}
 	
+	/**
+	 * Returns the floor the passenger wants to go.
+	 * 
+	 * @return floorToGo
+	 */
 	public int getFloorToGo() {
 		return floorToGo;
 	}
+	
+	/**
+	 * Returns true if the current floor of the Elevator is set.
+	 * 
+	 * @return true
+	 */
+	public boolean setFloorToGo(int newFloorNum) {
+		this.floorToGo = newFloorNum;
+		return true;
+		
+	}
+
 
 }
