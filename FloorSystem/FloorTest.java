f80ffa13e2a722b0bbb696bc975fe92378163849
Keeferf <keefer.belanger@gmail.com>
2023-02-25 15:18:20 -0500

    Test class for Floor.java

---
+++ FloorSystem/FloorTest.java
@@ -0,0 +1,60 @@
+package FloorSystem;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FloorTest {
+
+    @Test
+    void testGetFloorNum() {
+        Floor floor = new Floor(5);
+        assertEquals(5, floor.getFloorNum());
+    }
+
+    @Test
+    void testSetFloorNum() {
+        Floor floor = new Floor(5);
+        floor.setFloorNum(7);
+        assertEquals(7, floor.getFloorNum());
+    }
+
+    @Test
+    void testGetPeople() {
+        Floor floor = new Floor(5);
+        assertEquals(0, floor.getPeople());
+    }
+
+    @Test
+    void testSetPeople() {
+        Floor floor = new Floor(5);
+        floor.setPeople(3);
+        assertEquals(3, floor.getPeople());
+    }
+
+    @Test
+    void testGetUpLamp() {
+        Floor floor = new Floor(5);
+        assertFalse(floor.getUpLamp());
+    }
+
+    @Test
+    void testSetUpLamp() {
+        Floor floor = new Floor(5);
+        floor.setUpLamp(true);
+        assertTrue(floor.getUpLamp());
+    }
+
+    @Test
+    void testGetDownLamp() {
+        Floor floor = new Floor(5);
+        assertFalse(floor.getDownLamp());
+    }
+
+    @Test
+    void testSetDownLamp() {
+        Floor floor = new Floor(5);
+        floor.setDownLamp(true);
+        assertTrue(floor.getDownLamp());
+    }
+}
+
