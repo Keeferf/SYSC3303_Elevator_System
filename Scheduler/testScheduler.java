a70e45070ab9f9d42084e96284ec7660853821ae
mande <colinmandeville@cmail.carleton.ca>
2023-03-11 20:57:36 -0500

    Optimize scheduler

--- Scheduler/testScheduler.java
+++ Scheduler/testScheduler.java
@@ -13,41 +13,7 @@ import FloorSystem.FloorSubsystem;
  * Test Class for the Scheduler class
  * @author Colin Mandeville 101140289
  */
-class testScheduler {
-	@Test
-	void testAddUpEvents() {
-		Scheduler s = new Scheduler();
-		s.newRequest(new ElevatorEvent(this, "14:05:15", Direction.UP, 0, 2));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(1, s.getValidQueueLength());
-		s.newRequest(new ElevatorEvent(this, "18:38:21", Direction.UP, 1, 2));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(2, s.getValidQueueLength());
-		s.newRequest(new ElevatorEvent(this, "20:01:34", Direction.UP, 1, 3));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(3, s.getValidQueueLength());
-	}
-	
-	@Test
-	void testAddDownEvents() {
-		Scheduler s = new Scheduler();
-		s.newRequest(new ElevatorEvent(this, "14:05:15", Direction.DOWN, 4, 2));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(1, s.getValidQueueLength());
-		s.newRequest(new ElevatorEvent(this, "18:38:21", Direction.DOWN, 3, 0));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(2, s.getValidQueueLength());
-		s.newRequest(new ElevatorEvent(this, "20:01:34", Direction.DOWN, 2, 1));
-		assertEquals(1, s.getIncomingQueueLength());
-		s.validateRequest();
-		assertEquals(3, s.getValidQueueLength());
-	}
-	
+class testScheduler {	
 	@Test
 	void testProcessEvents() {
 		FloorSubsystem f = new FloorSubsystem();
